74c2808a1692e4f40118525857f4471e41c8a145
Shardy678 <tambov.j678@gmail.com>
2024-09-27 17:28:08 +0300

    rework logic

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,26 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation } from 'swiper/modules'
-import gsap from 'gsap'
 import 'swiper/css'
 import 'swiper/css/navigation'
+import gsap from 'gsap'
 
 const Timeline = () => {
-  const [activeTimelineIndex, setActiveTimelineIndex] = useState<number>(0)
-  const leftYearRef = useRef<HTMLDivElement>(null) // Reference for left year
-  const rightYearRef = useRef<HTMLDivElement>(null) // Reference for right year
+  const [activeSlide, setActiveSlide] = useState(0)
 
   const timelines = [
     {
       title: 'Наука',
-      titleYears: [2015, 2017],
+      titleYears: [2015, 2018],
       years: [
-        {
-          year: 2015,
-          events: ['Солнечное затмение в Африке'],
-        },
+        { year: 2015, events: ['Солнечное затмение в Африке'] },
         { year: 2016, events: ['Обнаружена галактика GN-z11'] },
         { year: 2017, events: ['Tesla представила грузовик Tesla Semi'] },
+        { year: 2018, events: ['Tesla представила Model 3'] },
       ],
     },
     {
@@ -39,50 +34,90 @@ const Timeline = () => {
         { year: 2024, events: ['Ещё одна выставка в 2024'] },
       ],
     },
+    {
+      title: 'История',
+      titleYears: [2024, 2028],
+      years: [
+        { year: 2024, events: ['Выставка 2024'] },
+        { year: 2028, events: ['Ещё одна выставка в 2028'] },
+      ],
+    },
+    {
+      title: 'Музыка',
+      titleYears: [2028, 2032],
+      years: [
+        { year: 2028, events: ['Выставка 2028'] },
+        { year: 2032, events: ['Ещё одна выставка в 2032'] },
+      ],
+    },
+    {
+      title: 'Кинематограф',
+      titleYears: [2032, 2036],
+      years: [
+        { year: 2032, events: ['Выставка 2032'] },
+        { year: 2036, events: ['Ещё одна выставка в 2036'] },
+      ],
+    },
   ]
+  const [years, setYears] = useState(timelines[activeSlide].titleYears)
+
+  const totalCircles = timelines.length
 
   useEffect(() => {
-    if (leftYearRef.current && rightYearRef.current) {
-      gsap.to(leftYearRef.current, {
-        innerText: timelines[activeTimelineIndex].titleYears[0],
-        duration: 1,
-        snap: { innerText: 1 }, // Snap to whole numbers
-      })
-      gsap.to(rightYearRef.current, {
-        innerText: timelines[activeTimelineIndex].titleYears[1],
-        duration: 1,
+    gsap.fromTo(
+      '.left-year',
+      { innerText: years[0] },
+      {
+        innerText: timelines[activeSlide].titleYears[0],
+        duration: 0.5,
         snap: { innerText: 1 },
-      })
-    }
-  }, [activeTimelineIndex])
+      }
+    )
+    gsap.fromTo(
+      '.right-year',
+      { innerText: years[1] },
+      {
+        innerText: timelines[activeSlide].titleYears[1],
+        duration: 0.5,
+        snap: { innerText: 1 },
+      }
+    )
+  }, [activeSlide])
 
   return (
-    <div className="timeline-slider">
-      <h1>Исторические даты</h1>
-      <div className="timeline-years" id="years">
-        <div ref={leftYearRef} className="year-left">
-          {timelines[activeTimelineIndex].titleYears[0]}
-        </div>
-        <div ref={rightYearRef} className="year-right">
-          {timelines[activeTimelineIndex].titleYears[1]}
-        </div>
+    <>
+      <div className="title-years">
+        <h1 className="left-year">{timelines[activeSlide].titleYears[0]}</h1>
+        <h1 className="right-year">{timelines[activeSlide].titleYears[1]}</h1>
       </div>
-      <Swiper
-        className="timeline-titles"
-        modules={[Navigation]}
-        slidesPerView={1}
-        navigation
-        onSlideChange={(swiper) => setActiveTimelineIndex(swiper.activeIndex)}
-      >
-        {timelines.map((timeline, index) => (
+      <p>{`0${activeSlide + 1}/06`}</p>
+
+      <div className="nav-buttons">
+        <button
+          onClick={() => setActiveSlide(Math.max(0, activeSlide - 1))}
+          className="nav-button prev"
+        >
+          <span className="chevron left"></span>
+        </button>
+        <button
+          onClick={() =>
+            setActiveSlide(Math.min(totalCircles - 1, activeSlide + 1))
+          }
+          className="nav-button next"
+        >
+          <span className="chevron right"></span>
+        </button>
+      </div>
+      <h3>{timelines[activeSlide].title}</h3>
+      <Swiper slidesPerView={3}>
+        {timelines[activeSlide].years.map((event, index) => (
           <SwiperSlide key={index}>
-            <div className="timeline-title">
-              <h3>{timeline.title}</h3>
-            </div>
+            <p>{event.year}</p>
+            <h3>{event.events}</h3>
           </SwiperSlide>
         ))}
       </Swiper>
-    </div>
+    </>
   )
 }
 
