2b115d09ff44170f3d54affa3f7957773fa5eebe
Shardy678 <tambov.j678@gmail.com>
2024-10-02 11:03:01 +0300

    refactor & reorganize

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -9,60 +9,74 @@ import { TimelinesData } from '../timelineTypes'
 import timelines from '../timelines.json'
 
 const Timeline: React.FC = () => {
-  const [timelinesData, setTimelines] = useState<TimelinesData>(timelines)
+  const [timelinesData] = useState<TimelinesData>(timelines)
   const [activeSlide, setActiveSlide] = useState(0)
   const [previousSlide, setPreviousSlide] = useState(0)
+  const [rotation, setRotation] = useState(0)
   const [isFirstLoad, setIsFirstLoad] = useState(true)
+
   const swiperRef = useRef<any>(null)
   const pRefs = useRef<(HTMLParagraphElement | null)[]>([])
 
   const totalCircles = timelinesData.timelines.length
-  const circles = Array.from({ length: totalCircles }, (_, index) => index + 1)
+  const circles = Array.from({ length: totalCircles })
   const radius = 265
-
-  const [rotation, setRotation] = useState(0)
   const anglePerDot = 360 / totalCircles
   const years = timelinesData.timelines[activeSlide].titleYears
 
   useEffect(() => {
-    // Animate year changes
-    gsap.fromTo(
-      '.left-year',
-      { innerText: years[0] - 1 },
-      {
-        innerText: years[0],
-        ease: 'power1.out',
-        duration: 0.75,
-        snap: { innerText: 1 },
-      }
-    )
-    gsap.fromTo(
-      '.right-year',
-      { innerText: years[1] - 1 },
-      {
-        innerText: years[1],
-        ease: 'power1.out',
-        duration: 0.75,
-        snap: { innerText: 1 },
-      }
-    )
+    animateYearChange()
+    positionDots()
+    animateCircleRotation()
+    setPreviousSlide(activeSlide)
+    setIsFirstLoad(false)
+  }, [activeSlide])
+
+  const animateYearChange = () => {
+    const animateYear = (selector: string, year: number) => {
+      gsap.fromTo(
+        selector,
+        { innerText: year - 1 },
+        {
+          innerText: year,
+          ease: 'power1.out',
+          duration: 0.75,
+          snap: { innerText: 1 },
+        }
+      )
+    }
+    animateYear('.left-year', years[0])
+    animateYear('.right-year', years[1])
+
     gsap.fromTo(
       '.swiper-container',
       { opacity: 0, y: 25 },
       { opacity: 1, y: 0, duration: 0.5, ease: 'power2.out' }
     )
+  }
 
-    positionDots()
+  const positionDots = () => {
+    circles.forEach((_, index) => {
+      const angle = (360 / totalCircles) * index - 60
+      const x = Math.cos((angle * Math.PI) / 180) * radius
+      const y = Math.sin((angle * Math.PI) / 180) * radius
 
+      gsap.set(`.dot:nth-child(${index + 1})`, {
+        x,
+        y,
+        transformOrigin: '50% 50%',
+        duration: 0,
+        ease: 'none',
+      })
+    })
+  }
+
+  const animateCircleRotation = () => {
     const totalSlides = totalCircles
-
     let slideDifference = activeSlide - previousSlide
 
-    if (slideDifference > totalSlides / 2) {
-      slideDifference -= totalSlides
-    } else if (slideDifference < -totalSlides / 2) {
-      slideDifference += totalSlides
-    }
+    if (slideDifference > totalSlides / 2) slideDifference -= totalSlides
+    else if (slideDifference < -totalSlides / 2) slideDifference += totalSlides
 
     const newRotation = rotation - slideDifference * anglePerDot
 
@@ -78,6 +92,7 @@ const Timeline: React.FC = () => {
         )
       },
     })
+
     gsap.to('.dot', { rotation: -newRotation, duration: 0, ease: 'none' })
 
     if (!isFirstLoad) {
@@ -89,44 +104,38 @@ const Timeline: React.FC = () => {
     }
 
     setRotation(newRotation)
-    setPreviousSlide(activeSlide)
-
-    setIsFirstLoad(false)
-  }, [activeSlide])
-
-  const positionDots = () => {
-    circles.forEach((_, index) => {
-      const angle = (360 / totalCircles) * index - 60
-      const x = Math.cos((angle * Math.PI) / 180) * radius
-      const y = Math.sin((angle * Math.PI) / 180) * radius
-
-      gsap.set(`.dot:nth-child(${index + 1})`, {
-        x: x,
-        y: y,
-        transformOrigin: '50% 50%',
-        duration: 0,
-        ease: 'none',
-      })
-    })
   }
 
-  const handleActiveSlideChange = (newSlide: number) => {
-    setActiveSlide(newSlide)
-    swiperRef.current.activeSlide = newSlide
-  }
+  const handleSlideChange = (newSlide: number) => setActiveSlide(newSlide)
+
+  const renderNavButton = (
+    direction: 'prev' | 'next',
+    disabled: boolean,
+    onClick: () => void
+  ) => (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`nav-button ${direction} ${disabled ? 'disabled' : ''}`}
+    >
+      <span
+        className={`chevron ${direction === 'prev' ? 'left' : 'right'}`}
+      ></span>
+    </button>
+  )
 
   return (
     <>
       <div className="container">
         <div className="circle-container">
-          {circles.map((circle, index) => (
+          {circles.map((_, index) => (
             <div
               key={index}
               className={`dot ${activeSlide === index ? 'active' : ''}`}
-              onClick={() => handleActiveSlideChange(index)}
+              onClick={() => handleSlideChange(index)}
             >
               <div className="dot-content">
-                <span>{circle}</span>
+                <span>{index + 1}</span>
                 <p ref={(el) => (pRefs.current[index] = el)}>
                   {timelinesData.timelines[activeSlide].title}
                 </p>
@@ -149,54 +158,26 @@ const Timeline: React.FC = () => {
           <p className="pagination">{`0${activeSlide + 1}/06`}</p>
 
           <div className="nav-buttons">
-            <button
-              onClick={() => {
-                const newIndex = Math.max(0, activeSlide - 1)
-                handleActiveSlideChange(newIndex)
-              }}
-              disabled={activeSlide === 0}
-              className={`nav-button prev ${
-                activeSlide === 0 ? 'disabled' : ''
-              }`}
-            >
-              <span className="chevron left"></span>
-            </button>
-            <button
-              onClick={() => {
-                const newIndex = Math.min(totalCircles - 1, activeSlide + 1)
-                handleActiveSlideChange(newIndex)
-              }}
-              disabled={activeSlide === totalCircles - 1}
-              className={`nav-button next ${
-                activeSlide === totalCircles - 1 ? 'disabled' : ''
-              }`}
-            >
-              <span className="chevron right"></span>
-            </button>
+            {renderNavButton('prev', activeSlide === 0, () =>
+              handleSlideChange(Math.max(0, activeSlide - 1))
+            )}
+            {renderNavButton('next', activeSlide === totalCircles - 1, () =>
+              handleSlideChange(Math.min(totalCircles - 1, activeSlide + 1))
+            )}
           </div>
 
           <Swiper
-            onInit={(swiper) => {
-              swiperRef.current = swiper
-            }}
+            onInit={(swiper) => (swiperRef.current = swiper)}
             className="myswiper"
             slidesPerView={3}
             breakpoints={{
-              1440: {
-                slidesPerView: 3.5,
-              },
-              968: {
-                slidesPerView: 2.5,
-              },
-              440: {
-                slidesPerView: 1.5,
-              },
+              1440: { slidesPerView: 3.5 },
+              968: { slidesPerView: 2.5 },
+              440: { slidesPerView: 1.5 },
             }}
             modules={[Navigation, Pagination]}
             navigation
-            pagination={{
-              clickable: true,
-            }}
+            pagination={{ clickable: true }}
           >
             {timelinesData.timelines[activeSlide].years.map((event, index) => (
               <SwiperSlide key={index}>
@@ -209,33 +190,18 @@ const Timeline: React.FC = () => {
           </Swiper>
         </div>
       </div>
+
       <div className="swiper-pagination"></div>
 
       <div className="mobile-nav-buttons">
         <p className="pagination">{`0${activeSlide + 1}/06`}</p>
         <div className="button-container">
-          <button
-            onClick={() => {
-              const newIndex = Math.max(0, activeSlide - 1)
-              handleActiveSlideChange(newIndex)
-            }}
-            disabled={activeSlide === 0}
-            className={`nav-button prev ${activeSlide === 0 ? 'disabled' : ''}`}
-          >
-            <span className="chevron left"></span>
-          </button>
-          <button
-            onClick={() => {
-              const newIndex = Math.min(totalCircles - 1, activeSlide + 1)
-              handleActiveSlideChange(newIndex)
-            }}
-            disabled={activeSlide === totalCircles - 1}
-            className={`nav-button next ${
-              activeSlide === totalCircles - 1 ? 'disabled' : ''
-            }`}
-          >
-            <span className="chevron right"></span>
-          </button>
+          {renderNavButton('prev', activeSlide === 0, () =>
+            handleSlideChange(Math.max(0, activeSlide - 1))
+          )}
+          {renderNavButton('next', activeSlide === totalCircles - 1, () =>
+            handleSlideChange(Math.min(totalCircles - 1, activeSlide + 1))
+          )}
         </div>
       </div>
     </>
